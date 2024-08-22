24deb4e7f62432c36e383b6930d1b76af1238483
Ignasiya <ignasiya93@gmail.com>
2024-08-22 19:20:39 +0300

    Небольшие правки

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const elCall = document.querySelector('#callModal')
 
 const elShadingBackground = document.querySelector('#shadingBackground')
 
-const elsNavigateShowToggle = document.querySelectorAll('.navbar__show-toggle')
+const elsNavigateShowToggle = document.querySelectorAll('.actions-slide__show-toggle')
 
 const swiperElements = document.querySelectorAll('.swiper')
 
@@ -63,7 +63,7 @@ elsNavigateShowToggle?.forEach(el => {
   el.addEventListener('click', () => {
     const elList = el.previousElementSibling
     el.classList.toggle('show-toggle--active')
-    elList.classList.toggle('navbar__list--show-all')
+    elList.classList.toggle('actions-slide__list--show-all')
 
     if (el.classList.contains('show-toggle--active')) {
       el.lastElementChild.textContent = 'Скрыть'
